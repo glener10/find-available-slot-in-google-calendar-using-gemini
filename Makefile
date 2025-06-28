e1e9337d83abaf452081a85f63df0a436bda46d7
glener10 <glenerpizzolato@gmail.com>
2025-06-28 10:10:20 -0300

    feat: update makefile to exec with arguments

--- Makefile
+++ Makefile
@@ -1,17 +1,18 @@
-SHELL := /bin/bash
+SHELL := /bin/zsh
 
 .PHONY: run clean
 
 run:
+	@# Este bloco inteiro é executado em um único shell por causa das barras invertidas
 	@echo "Checking for 'venv' virtual environment..."
 	@if [ ! -d "venv" ]; then \
 		echo "'venv' virtual environment not found. Creating and installing dependencies..."; \
 		python3 -m venv venv; \
-		source venv/bin/activate && venv/bin/pip3 install -r requirements.txt; \
+		source venv/bin/activate && pip3 install -r requirements.txt; \
 		echo "Installation complete!"; \
-	fi
-	@echo "Activating virtual environment and running the application..."
-	source venv/bin/activate && venv/bin/python3 main.py $(ARGS)
+	fi; \
+	echo "Activating virtual environment and running the application..."; \
+	source venv/bin/activate && python3 main.py $(ARGS)
 
 clean:
 	@echo "Removing virtual environment and temporary files..."
--- src/utils/args.py
+++ src/utils/args.py
@@ -6,7 +6,7 @@ def get_args():
   parser.add_argument('-e', '--end', required=False, default='6:00 PM', help='attendance time end (default: 6:00 PM)')
   parser.add_argument('-d', '--duration', required=False, default='60', help='event duration in minutes (default: 60)')
   parser.add_argument('-n', '--name', required=False, default='Event created by Gemini', help='event name (default: "Event created by Gemini")')
-  parser.add_argument('-id', '--id', required=False, default='primary', help='calendar id (default: "primary")')
+  parser.add_argument('--id', required=False, default='primary', help='calendar id (default: "primary")')
   parser.add_argument('-w', '--waiting', required=False, default='30', help='waiting time to search events (default: 30)')
   parser.add_argument('-i', '--invites', required=False, help='email to invite to the event, pass N emails separates by , (optional)')
   args = parser.parse_args()
